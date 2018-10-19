bf81fd7942fabaa3d7fb6d6d32efddd855b51cb5
Dev-Zero1 <DevMail.exe@gmail.com>
2018-10-19 11:12:27 -0700

    Initial Commit

---
+++ tipped/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipped
+//
+//  Created by administrator on 10/19/18.
+//  Copyright © 2018 administrator. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
